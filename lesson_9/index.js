b789df645956ce9cfa752c6326ed4f8a5c00c100
Module <bogdan.alexsandrovich@gmail.com>
2019-06-18 13:37:09 +0300

    HW-9.1

--- lesson_9/index.js
+++ lesson_9/index.js
@@ -1,1 +1,27 @@
 "use strict";
+
+const div = document.createElement('div');
+
+div.style.height = '200%';
+
+function newRandomColor() {
+    let color = `rgb(${Math.round(Math.random() * 255)}, ${Math.round(Math.random() * 255)}, ${Math.round(Math.random() * 255)})`;
+    return color;//, console.log(color);
+}
+
+//newRandomColor();
+
+function waitForScroll() {
+    return new Promise((resolve) => {
+        window.addEventListener('scroll', resolve);
+    });
+}
+
+function drawOnScroll(){
+    waitForScroll()
+        .then(() => {
+            div.style.background = newRandomColor();
+        });
+} 
+window.waitForScroll = waitForScroll;
+window.drawOnScroll = drawOnScroll;
