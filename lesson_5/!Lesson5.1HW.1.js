cf86ff74266c6d7ac140abd1b331e17f9acb0b71
Bogdan Parshyn <b.parshyn@dms.local>
2019-07-11 16:05:57 +0300

    HW_2

--- lesson_5/!Lesson5.1HW.1.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-document.getElementById('OpenButton').onclick = function(){
-  document.getElementById('div').style.display = 'block';          
-}
-document.getElementById('Hide').onclick = function (){
-  document.getElementById('div').style.display = 'none';           
-}
--- lesson_5/node.js
+++
@@ -1,4 +0,0 @@
-const some = require('./addon.js');
-
-some.print(8);
-some.print2(7);
